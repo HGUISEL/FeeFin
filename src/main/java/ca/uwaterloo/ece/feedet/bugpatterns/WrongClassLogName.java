f9298d4b20e7a8a6e298db88ef0b46baeabee816
JC <jc.nam@uwaterloo.ca>
2017-07-06 16:53:50 -0400

    #12 Initial commit for WrongClassLogName

---
+++ src/main/java/ca/uwaterloo/ece/feedet/bugpatterns/WrongClassLogName.java
@@ -0,0 +1,43 @@
+package ca.uwaterloo.ece.feedet.bugpatterns;
+
+import java.util.ArrayList;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jgit.lib.Repository;
+
+import ca.uwaterloo.ece.feedet.DetectionRecord;
+import ca.uwaterloo.ece.feedet.utils.JavaASTParser;
+
+public class WrongClassLogName extends Bug {
+
+	public WrongClassLogName(String prjName,JavaASTParser ast, String id, String path, Repository repo){
+		initialize(prjName,ast,id,path,repo,this.getClass().getSimpleName());
+	}
+	
+	@Override
+	protected String getDescription() {
+		return "LogFactory loads a wrong class.";
+	}
+	
+	@Override
+	public ArrayList<DetectionRecord> detect() {
+		// Detection results are stored in this ArrayList
+		ArrayList<DetectionRecord> listDetRec = new ArrayList<DetectionRecord>();
+		
+		// An example loop to get some AST nodes to analyze
+		for(MethodInvocation methodInv:wholeCodeAST.getMethodInvocations()){
+			
+			if(!methodInv.toString().contains("class.getName()")) continue;
+			
+			if(!(methodInv.getParent() instanceof MethodInvocation
+					&&methodInv.getParent().toString().toLowerCase().contains("log"))) continue;
+			
+			// get Line number
+			int lineNum = wholeCodeAST.getLineNum(methodInv.getStartPosition());
+			
+			listDetRec.add(new DetectionRecord(bugName, getDescription(), projectName, id, path, lineNum, methodInv.getParent().toString(), false, false));	
+		}
+		
+		return listDetRec;
+	}
+}
