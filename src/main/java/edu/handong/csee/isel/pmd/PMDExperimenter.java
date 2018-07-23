c09db4364b31776dee6748adb0828efe45341f80
JC <JC jc@lifove.net>
2018-07-23 14:30:54 +0900

    Add PMDExperimenter (initial)

---
+++ src/main/java/edu/handong/csee/isel/pmd/PMDExperimenter.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.isel.pmd;
+
+public class PMDExperimenter {
+
+	public static void main(String[] args) {
+		PMDExperimenter experimenter = new PMDExperimenter();
+		experimenter.run(args);
+	}
+
+	private void run(String[] args) {
+		
+	}
+}
