6140e3485096eeb62124470cbd3b7e62ad4f4e71
JC <JC jc@lifove.net>
2018-07-26 17:19:26 +0900

    Fix a bug for deadlock (standard stream need to be read without trhead but not for err stream. So switched the position)

--- src/test/java/edu/handong/csee/isel/pmd/PMDExperimenterTest.java
+++ src/test/java/edu/handong/csee/isel/pmd/PMDExperimenterTest.java
@@ -11,5 +11,11 @@ public class PMDExperimenterTest {
 		String[] args = {"-d", "C:\\Users\\jaech\\git\\FeeFin"};
 		PMDExperimenter.main(args);
 	}
+	
+	@Test
+	public void testApexCore() {
+		String[] args = {"-d", "C:\\Users\\jaech\\git\\apex-core", "-s", "2016-07-15 17:26:35", "-e","2016-07-19 05:48:13","-v"};
+		PMDExperimenter.main(args);
+	}
 
 }
