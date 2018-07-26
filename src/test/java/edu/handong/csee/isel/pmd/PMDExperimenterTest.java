0ac586dd205aa26b560332296260bb70fe74df6e
JC <JC jc@lifove.net>
2018-07-26 16:33:15 +0900

    Add PMDExperimenterTest

---
+++ src/test/java/edu/handong/csee/isel/pmd/PMDExperimenterTest.java
@@ -0,0 +1,15 @@
+package edu.handong.csee.isel.pmd;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PMDExperimenterTest {
+
+	@Test
+	public void test() {
+		String[] args = {"-d", "C:\\Users\\jaech\\git\\FeeFin"};
+		PMDExperimenter.main(args);
+	}
+
+}
